f31678448e79af768f7d1a659c1f6e0bc021aeb4
Rodrigo Paschoaletto <rodrigopaschoaletto@gmail.com>
2018-10-20 08:51:12 -0300

    ⭐️ Courses - Done

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,10 @@
 class CoursesController < InheritedResources::Base
   before_action :authenticate_user!
+  actions :all, except: :show
 
   private
 
-    def course_params
-      params.require(:course).permit(:name, :teacher, :teacher_email, :observations)
-    end
+  def course_params
+    params.require(:course).permit(:name, :teacher, :teacher_email, :observations)
+  end
 end
--- app/views/courses/_course.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! course, :id, :name, :teacher, :teacher_email, :observations, :created_at, :updated_at
-json.url course_url(course, format: :json)
